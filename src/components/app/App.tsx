60d387fb51beeee3f28c6e33ecef759c0e5f3355
Bassem Ghazouani <bassem.ghazouani@hotmail.com>
2024-02-05 19:18:47 +0100

    fix: run the formater for the project

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
+import React from "react";
 
 export const App: React.FunctionComponent = () => {
-  return (
-    <div>
-      Hello test of the react app
-    </div>
-  );
-}
+  return <div>Hello test of the react app</div>;
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,20 @@
 // External dependencies imports
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 // Internal dependencies imports
-import { GlobalStyle } from './globalStyle';
-import { App } from './components/app/App';
-import reportWebVitals from './reportWebVitals';
+import { GlobalStyle } from "./globalStyle";
+import { App } from "./components/app/App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <GlobalStyle />
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
