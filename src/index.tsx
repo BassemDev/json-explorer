d9acdede04931fff186af2e1b6e6839c5e923ac1
Bassem Ghazouani <bassem.ghazouani@hotmail.com>
2024-02-05 19:15:31 +0100

    refactor: re-adjust the project structure and add description for the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
+// External dependencies imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+
+
+// Internal dependencies imports
+import { GlobalStyle } from './globalStyle';
+import { App } from './components/app/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,6 +13,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
