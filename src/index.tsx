7fa31b650480a238826a295a2b67d73b855a8480
Bassem Ghazouani <bassem.ghazouani@hotmail.com>
2024-02-05 19:19:21 +0100

    feat: introduce the two huskies allowing to format the project before pushing

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 
 // Internal dependencies imports
 import { GlobalStyle } from "./globalStyle";
-import { App } from "./components/app/App";
+import {App} from "./components/app/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
